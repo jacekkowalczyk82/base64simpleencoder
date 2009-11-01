b00168b9c120fabe8ee31084c9ed4eb8ed28631c
jacekkowalczyk82 <jacekkowalczyk82@8e79c3e2-874e-11de-9934-f1fc26e2a4cc>
2009-11-01 18:58:14 +0000

    simple application that is able to code  strings in base 64 algorithm

---
+++ trunk/b64se.bat
@@ -0,0 +1,4 @@
+@echo off
+
+java -cp build/classes;. pl.interia.w.kowalczyk.jacek.Base64SimpleEncoder %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
---
+++ trunk/b64tool.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem java -cp build/classes;. pl.interia.w.kowalczyk.jacek.Base64SimpleEncoderTool 
+java -jar Base64SimpleEncoder.jar 
+
+pause
